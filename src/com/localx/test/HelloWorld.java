6f0925a686599a88393aa28dca3777550364ce8b
huaguohao <810913476@qq.com>
2026-06-16 10:12:54 +0800

    hello world

---
+++ src/com/localx/test/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.localx.test;
+// first java app
+public class HelloWorld {
+    // main app
+    public static void main(String[] args) {
+        // out Hello Wold
+        System.out.println("Hello World");
+    }
+}
